9c410cd377885749101a49967f010630d3a5f357
thuanan7 <thuanan7@gmail.com>
2024-06-15 14:02:58 +0700

    [Catalog] Create Product init

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Catagory, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid ProductId);
+
+    public class CreateProductCommandHandler
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Catagory = command.Catagory,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // TODO: save product entity to database
+
+            // return CreateProductResult with product id
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
