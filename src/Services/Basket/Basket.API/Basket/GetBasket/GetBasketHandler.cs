a68eccfea3da140c895c0c3a8c467444ca3c54cb
thuanan7 <thuanan7@gmail.com>
2024-06-25 10:02:33 +0700

    Template for [Basket] services endpoint and handler

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,15 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //TODO: get basket from repository
+            // var basket = await _repository.GetBasketAsync(query.UserName);
+
+            return new GetBasketResult(new ShoppingCart(query.UserName));
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,29 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record struct StoreBasketCommand(string UserName, ShoppingCart Cart) 
+        : ICommand<StoreBasketResult>;
+    public record struct StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle
+            (StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            //TODO: store basket in database (use Marten upsert)
+            //TODO: update cache
+
+            return new StoreBasketResult(command.UserName);
+        }
     }
 }
