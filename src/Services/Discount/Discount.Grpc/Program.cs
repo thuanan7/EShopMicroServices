1e98d08e10ab066d293efbd3d5dd8bab2285f985
thuanan7 <thuanan7@gmail.com>
2024-06-28 09:10:10 +0700

    [Discount.Grpc] Init DiscountService

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,6 @@
 
+using Discount.Grpc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +9,8 @@ builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
-
-
 // Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment())
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,29 @@
+﻿using Discount.Grpc.Protos;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            //TODO: GetDiscount from db
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
