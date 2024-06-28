b477cfe8e357571927dfd728794f8b43022af90d
thuanan7 <thuanan7@gmail.com>
2024-06-28 10:23:11 +0700

    [Discount.Grpc] Create DiscountContext and create coupons table using ef core sqlite

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,11 +1,18 @@
 
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
+
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
 
